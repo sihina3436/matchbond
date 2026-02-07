4aae2e5aa0a0f00932617597a4405dd3e0492068
sihina3436 <nimnadasihina@gmail.com>
2026-02-07 23:26:01 +0530

    Update the router file

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -1,13 +1,38 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
+import ForgotPassword from "../pages/ForgotPassword";
+import VerifyOTP from "../pages/VerifyOTP";
+import Feed from "../pages/Feed";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
         children: [
-            {path:"/home", element:<Home /> }
+            {path:"/home", element:<Home /> },
+            {path:"/feed", element:<Feed /> }
         ]
-    }
+    },
+    {
+        path:"/signin",
+        element: <SignIn />
+    },
+    {
+        path:"/signup",
+        element: <SignUp />
+    },
+    {
+        path:"/forgot-password",
+        element: <ForgotPassword />
+    },
+    {
+        path:"/verify-otp",
+        element: <VerifyOTP />
+    },
+
+
+
 ]);
