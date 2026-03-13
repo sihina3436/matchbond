b2206080490b1cbe3109720d81e98cbb391b488b
sihina3436 <nimnadasihina@gmail.com>
2026-03-14 01:20:22 +0530

    refactor(redux): improve store setup in store.ts

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,10 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { emptyReducer } from "./emptyReducer";
+import userReducer from "./userAuth/userAuthSlice"; 
+import { userAuthAPI } from "./userAuth/userAuthAPI"; 
+import adminAuthReducer from "./adminAuth/adminAuthSlice";
+import { adminAuthAPI } from "./adminAuth/adminAuthApi";
+import { chatAPI } from "./chatRequests/chatApi";
+import { imageAPI } from "./image/imageAPI";
+import { postAPI } from "./post/postAPI";
+import { postLikeAPI } from "./postLike/postLikeAPI";
 
 export const store = configureStore({
-    reducer:{ empty: emptyReducer }
+  reducer: {
+    user: userReducer,
+    admin : adminAuthReducer,
+
+    [userAuthAPI.reducerPath]: userAuthAPI.reducer,
+    [adminAuthAPI.reducerPath]: adminAuthAPI.reducer,
+    [chatAPI.reducerPath]: chatAPI.reducer,
+    [imageAPI.reducerPath]: imageAPI.reducer,
+    [postAPI.reducerPath]: postAPI.reducer,
+    [postLikeAPI.reducerPath]: postLikeAPI.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
+        userAuthAPI.middleware,
+        adminAuthAPI.middleware,
+        chatAPI.middleware,
+        imageAPI.middleware,
+        postAPI.middleware,
+        postLikeAPI.middleware
+
+    ),
 });
 
+// Types for TypeScript
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
