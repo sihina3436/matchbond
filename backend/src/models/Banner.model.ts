74ff145878c545e863848f4de2f4e5fc1d702fd7
sihina3436 <nimnadasihina@gmail.com>
2026-03-15 23:51:21 +0530

    feat(models): create Banner.model.ts for banner schema

---
+++ backend/src/models/Banner.model.ts
@@ -0,0 +1,36 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+
+// Interface
+export interface IBanner extends Document {
+  bannerTitle: string;
+  bannerImageUrl: string;
+  type: "offer" | "review" ;
+
+}
+
+// Schema
+const BannerSchema = new Schema<IBanner>(
+  {
+    bannerTitle: {
+        type: String,
+        required: true,
+    },
+    bannerImageUrl: {
+        type: String,
+
+    },
+    type:{
+        type: String,
+        enum: ["offer", "review"],
+        default: "offer"
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Model
+const Banner = mongoose.model<IBanner>("Banner", BannerSchema);
+export default Banner;
